9ecdf3453b3de4045781cfd80f35ac75055a28d4
zhang <623239185@qq.com>
2018-07-31 17:23:17 +0800

    poller的基本框架

--- poller.cpp
+++ poller.cpp
@@ -3,3 +3,56 @@
 //
 
 #include "poller.h"
+
+void  Channel::handleEvents()
+{
+    if (revents_ & kWriteEvent) {
+        if (writeCallbackFunc_) {
+            writeCallbackFunc_();
+        }
+    }
+
+    if (revents_ & kReadEvent) {
+        if (readCallbackFunc_) {
+            readCallbackFunc_();
+        }
+    }
+
+    if (revents_ & kErrorEvent) {
+        if (errorCallbackFunc_) {
+            errorCallbackFunc_();
+        }
+    }
+
+}
+
+void PollPoller::poll()
+{
+    //准备poll需要的监听时间
+    pollfds_.clear();
+    for (const auto& p : pollChannels_) {
+        struct pollfd ev;
+        ev.fd = p.first;
+        ev.events = (short)p.second->getEvents();
+        pollfds_.push_back(ev);
+    }
+
+    int ret = poll(pollfds_.data(), pollfds_.size(), kIntervalTime);
+    if (ret < 0) {
+        perror("poll error");
+    }
+
+    //提取有事件发生的Channel
+    activeChannels_.clear();
+    for (const auto & ev : pollfds_) {
+        if (ev.revents & (POLLOUT | POLLIN | POLLPRI | POLLERR | POLLHUP | POLLNVAL)) {
+            Channel * ch = pollChannels_[ev.fd];
+            ch->setRevents(ev.revents);
+            activeChannels_.push_back(ch);
+        }
+    }
+
+    //todo:处理定时器超时
+
+    handleEvents();
+}
--- poller.h
+++ poller.h
@@ -6,20 +6,122 @@
 #define COROUTLINE_POLLER_H
 
 #include <poll.h>
+#include <functional>
+#include <vector>
+#include <unordered_map>
 
+using EventCallbackFunc = std::function<void ()>;
+using TimeoutCallbackFunc = std::function<void ()>;
+
+class poller;
+
+class Channel
+{
+public:
+    Channel(poller* po, int event) : poller_(po), events_()
+    {
+    }
+
+    Channel(const Channel&) = delete;
+    Channel& operator=(const Channel&) = delete;
+
+    int getEvents()
+    {
+        return events_;
+    }
+
+    void setRevents(int revents)
+    {
+        revents_ = revents;
+    }
+
+    void enableWirte()
+    {
+        events_ |= kWriteEvent;
+    }
+
+    void enableRead()
+    {
+        events_ |= kReadEvent;
+    }
+
+    void disableWrite()
+    {
+        events_ &= ~kWriteEvent;
+    }
+
+    void disableRead()
+    {
+        events_ &= ~kReadEvent;
+    }
+
+    int getFd()
+    {
+        return fd_;
+    }
+
+    void handleEvents();
+
+private:
+    const static int kNoEvent = 0;
+#ifdef  __CYGWIN__
+    const static int kReadEvent = POLLIN | POLLPRI;
+    const static int kWriteEvent = POLLOUT;
+    const static int kErrorEvent = POLLERR;
+#else
+    //todo:linux 上使用Eepoll来实现
+    const static int kReadEvent = EPOLLIN;
+    const static int kWriteEvent = EPOLLOUT;
+#endif
+    poller * poller_;
+    int events_;
+    int revents_;
+    int fd_;
+    EventCallbackFunc readCallbackFunc_;
+    EventCallbackFunc writeCallbackFunc_;
+    EventCallbackFunc errorCallbackFunc_;
+};
+//给协程库用的poller都是在一个线程用的，不用考虑并发
 class poller
 {
+public:
+    virtual void poll() = 0;
+    virtual void addChannel(Channel* ch) = 0;
+    virtual void removeChannel(Channel* ch) = 0;
 
+protected:
+    std::vector<Channel*> activeChannels_;
+    static const int kIntervalTime = 1;
 };
 
 class PollPoller : public poller
 {
+public:
+    virtual void poll();
+    virtual void handleEvents();
+    virtual void addChannel(Channel* ch)
+    {
+        pollChannels_.insert(std::pair<int,Channel*>(ch->getFd(), ch));
+    }
 
+    virtual void removeChannel(Channel* ch)
+    {
+        if (pollChannels_.end() != pollChannels_.find(ch->getFd())) {
+            pollChannels_.erase(ch->getFd());
+        }
+    }
+
+private:
+    std::vector<struct pollfd> pollfds_;
+    std::unordered_map<int, Channel*> pollChannels_;
 };
 
+//tood:finish it
 class EpollPoller : public poller
 {
+public:
 
+private:
 };
 
 
